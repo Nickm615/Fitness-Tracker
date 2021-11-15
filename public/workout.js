02c7c294b18a0d23e34adfaa1503318360b0ccdf
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-11-15 17:39:42 -0600

    Added aggregation to get route, fixed references in getLastWorkout

--- public/workout.js
+++ public/workout.js
@@ -1,3 +1,5 @@
+
+
 async function initWorkout() {
   const lastWorkout = await API.getLastWorkout();
   console.log("Last workout:", lastWorkout);
@@ -7,10 +9,10 @@ async function initWorkout() {
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
     const workoutSummary = {
-      date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.totalDuration,
-      numExercises: lastWorkout.exercises.length,
-      ...tallyExercises(lastWorkout.exercises)
+      date: formatDate(lastWorkout[0].day),
+      totalDuration: lastWorkout[0].totalDuration,
+      numExercises: lastWorkout[0].exercises.length,
+      ...tallyExercises(lastWorkout[0].exercises)
     };
 
     renderWorkoutSummary(workoutSummary);
@@ -82,3 +84,5 @@ function renderNoWorkoutText() {
 }
 
 initWorkout();
+
+//where does this aggregate go??
--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,34 @@ const router = require("express").Router();
 const Workout = require('../models/Workout.js');
 
 //GET last workout (getLastWorkout())
-router.get("/api/workouts", (req, res) => {
-    Workout.findOne({})
-        .sort({day: -1})
-        .then(dbWorkout => {
-            console.log(dbWorkout)
-            res.json(dbWorkout)
-        });
+router.get("/api/workouts/range", (req, res)=>{
+    Workout.aggregate([
+        {
+          $addFields: {
+            "totalDuration": { $sum: "$exercises.duration"}
+          }
+        }
+      ])
+      .then(data =>{
+        res.json(data)
+      })
+        
+      
+// console.log('Field added')
 })
 
+router.get("/api/workouts", (req, res) => {
+Workout.aggregate([{
+$addFields:{totalDuration: { $sum: "$exercises.duration"}}
+},
+{$sort: {day: -1}},
+{$limit: 1}
+])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 module.exports = router
