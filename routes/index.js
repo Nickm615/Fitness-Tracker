83ce3019b945587913751fc1af6321dfb8380302
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-11-16 18:16:02 -0600

    deploy test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
-const express = require("express");
 const router = require('express').Router();
-const Workout = require('../models/Workout');
 const path = require("path");
 const apiRoutes = require('./api');
 
@@ -14,6 +12,7 @@ router.get('/exercise', (req, res)=>{
 router.get('/stats', (req,res)=>{
     res.sendFile(path.join(__dirname, '../public/stats.html'))
 })
+
 
 module.exports = router
 
