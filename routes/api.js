7926a46d19ba313fd465af1bb76876bd9fc69a29
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-11-18 09:19:16 -0600

    test deploy

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Workout } = require('../models/Workout');
+const  Workout = require('../models/Workout');
 
 
 router.post('/workouts', ({body}, res)=>{
